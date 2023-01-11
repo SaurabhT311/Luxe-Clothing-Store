ecd66b23582edba02fe4d54dfff979347ff381d4
Saurabh Tripathi <saurabh.tripathi@magicedtech.com>
2023-01-11 15:30:07 +0530

    Added category-item and categories components for dashboard

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import CategoryItem from './components/category-item/category-item';
+// import './categories.scss';
+import Directory from './components/directory/directory';
+
+
 const App = () => {
 
   // An array that contains object that reflect what we have in there.
@@ -5,42 +10,34 @@ const App = () => {
     {
       id: 1,
       title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png'
     },
     {
       id: 2,
       title: 'Jackets',
+      imageUrl: 'https://e7.pngegg.com/pngimages/587/966/png-clipart-flight-jacket-ma-1-bomber-jacket-clothing-canada-goose-jacket-thumbnail.png'
     },
     {
       id: 3,
       title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png'
     },
     {
       id: 4,
       title: 'Womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/women.png'
+
     },
     {
       id: 5,
       title: 'Mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'
     }
   ]
 
   return (
-    <div className="categories-container">
-      {/* map and de-structured the title from category 
-      categories.map((category) turns to => categories.map({title})
-      */}
-      {categories.map(({ title }) => (
-        <div className="category-container" >
-          {/* <img /> */}
-          < div className="category-body-container" >
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div >
-      ))}
-
-    </div>
+    <Directory categories={categories}/>
   );
-}
+};
 
 export default App;

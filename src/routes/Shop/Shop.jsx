c903d80ce39853493156d2ded23e5ac4bdb242e0
Saurabh Tripathi <tripathisaurabh.311@gmail.com>
2023-06-04 18:28:55 +0530

    Added productCard component and shop component in the project.

--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -1,14 +1,14 @@
 import { useContext } from "react";
 import { ProductContext } from "../../contexts/productContext";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import './Shop.scss';
 
 const Shop = () => {
     const { products } = useContext(ProductContext)
     return (
-        <div>
-            {products.map(({id,name}) => (
-                <div key={id}>
-                    <h1>{name}</h1>
-                </div>
+        <div className="products-container">
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product}/>
             ))}
         </div>
     )
